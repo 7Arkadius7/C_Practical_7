e2483fafba432097f9526627a374257317860341
Sergey <motor-person@yandex.ru>
2022-10-26 13:05:55 +0400

    Сделали Task_48

--- Task_48/Program.cs
+++ Task_48/Program.cs
@@ -1,15 +1,16 @@
-﻿Console.Clear();
+﻿//Task_48
+
+Console.Clear();
 
 
 int[,] CreateMatrix(int rows, int colums, int min, int max) // метод создания двухмерного массива со случайными числами
 {
     int[,] matrix = new int[rows, colums];
-    var rnd = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++) // matrix.GetLength(1) - размер длинны матрицы по строкам (rows)
     {
         for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(0) - размер длинны матрицы по столбцам (colums)
         {
-            matrix[i, j] = rnd.Next(min, max + 1);
+            matrix[i, j] = i + j;
         }
     }
     return matrix;
@@ -22,15 +23,15 @@ void PrintMatrix(int[,] matr) // создаем метод
         System.Console.Write(("["));
         for (int j = 0; j < matr.GetLength(1); j++) // где где matrix - название матрицы, а .GetLength(1) - количество строк в данной матрице (1)
         {
-            if (j < matr.GetLength(1) - 1) System.Console.Write($"{matr[i, j], 4}, "); // выводим данные из определенного столбца (i) и строки (j).
-                                                                                    // i берется из первого цикла, j из подцикла
-            else System.Console.Write($"{matr[i, j], 4}");
+            if (j < matr.GetLength(1) - 1) System.Console.Write($"{matr[i, j],4}, "); // выводим данные из определенного столбца (i) и строки (j).
+                                                                                      // i берется из первого цикла, j из подцикла
+            else System.Console.Write($"{matr[i, j],4}");
         }
         System.Console.WriteLine(("]"));
     }
 }
 
-int[,] matrix = CreateMatrix(3, 4, 1, 100); // вводим размер матрицы данных
+int[,] matrix = CreateMatrix(7, 7, 1, 100); // вводим размер матрицы данных
 PrintMatrix(matrix); // выводим сгенерированную матрицу
 System.Console.WriteLine(); // переход на новую строку
 
