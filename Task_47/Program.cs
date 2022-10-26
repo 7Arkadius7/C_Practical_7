da1b2e804a61f9fa403ade4073203e0c697ef367
Sergey <motor-person@yandex.ru>
2022-10-26 14:30:14 +0400

    Решили Task 47

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите количество строк матрицы");
+int rowsMatrix = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите количество столбцов матрицы");
+int columsMatrix = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите минимально возможный элемент матрицы");
+int minimal = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите максимально возможный элемент матрицы");
+int maximal = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = CreateMatrix(rowsMatrix, columsMatrix, minimal * 10, maximal * 10); // вводим размер матрицы данных
+
+
+double[,] CreateMatrix(int rows, int colums, int min, int max)
+{
+    double[,] matrix = new double[rows, colums];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(rnd.Next(min, max + 1)) / 10; ;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        System.Console.Write(("["));
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j < matr.GetLength(1) - 1) System.Console.Write($"{matr[i, j],4}, ");                                                                                       // i берется из первого цикла, j из подцикла
+            else System.Console.Write($"{matr[i, j],4}");
+        }
+        System.Console.WriteLine(("]"));
+    }
+}
+
+PrintMatrix(matrix);
+
+
